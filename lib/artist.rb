7f705ece1a8dbb936228f9b9bae9d50c02ddad2e
Ryan Parrish <rino3717@gmail.com>
2020-04-08 03:38:12 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+class Artist
+  
+    attr_accessor :name
+  
+   @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all 
+  end
+  
+  def songs
+    self.songs
+  end 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+end
